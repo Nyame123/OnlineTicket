6a3a17e33033b79321c82183fdf6dacf31edea37
nyame <nyamebismark12.nb@gmail.com>
2022-05-25 18:41:00 +0000

    adding database module to dagger

--- app/src/main/java/com/bismark/onlineticket/di/DatabaseModule.kt
+++ app/src/main/java/com/bismark/onlineticket/di/DatabaseModule.kt
@@ -10,18 +10,27 @@ import javax.inject.Singleton
 
 @Module
 class DatabaseModule constructor(
-    applicationContext: Context
+    private val applicationContext: Context
 ) {
 
     @Singleton
     @Provides
-    fun provideRoomDatabase(@ApplicationContext applicationContext: Context): TicketDatabase =
+    fun provideContext(): Context = applicationContext
+
+    @Singleton
+    @Provides
+    fun provideRoomDatabase(
+        applicationContext: Context
+    ): TicketDatabase =
         Room.databaseBuilder(applicationContext, TicketDatabase::class.java, DATABASE_NAME)
             .fallbackToDestructiveMigration()
+            .allowMainThreadQueries()
             .build()
 
     @Singleton
     @Provides
     fun provideTicketProvider(ticketDatabase: TicketDatabase): RoomTicketProvider =
         RoomTicketProviderImpl(ticketDatabase)
+
+
 }
